8d65952fa5108d9639b2595eb004e25b4ef5793e
Jeremy-PR <jeremy.pelisse@gmail.com>
2024-11-19 13:43:31 +0100

    all done

--- Exercice_3/script.js
+++ Exercice_3/script.js
@@ -1,9 +1,28 @@
-const green = document.querySelector('#green');
-const red = document.querySelector('#red');
-const blue = document.querySelector('#blue');
-const texte = document.querySelector('#text');
+// const green = document.querySelector('#green');
+// const red = document.querySelector('#red');
+// const blue = document.querySelector('#blue');
+// const texte = document.querySelector('#text');
 
-green.addEventListener('click' , handleAddGreen);
-red.addEventListener('click' , handleAddGreen);
-blue.addEventListener('click' , handleAddGreen);
+// green.addEventListener('click' , handleAddGreen);
+// red.addEventListener('click' , handleAddGreen);
+// blue.addEventListener('click' , handleAddGreen);
 
+
+
+
+
+
+
+
+const colorButtons = document.querySelectorAll('.color');
+const textElement = document.querySelector('#text');
+
+
+colorButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+
+        const color = this.classList[1]; 
+
+        textElement.style.color = color;
+    });
+});
