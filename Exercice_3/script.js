ec120d923c898fc170336c828dc01ef9c9f14412
Jeremy-PR <jeremy.pelisse@gmail.com>
2024-11-19 13:35:01 +0100

    exo 4 done

---
+++ Exercice_3/script.js
@@ -0,0 +1,9 @@
+const green = document.querySelector('#green');
+const red = document.querySelector('#red');
+const blue = document.querySelector('#blue');
+const texte = document.querySelector('#text');
+
+green.addEventListener('click' , handleAddGreen);
+red.addEventListener('click' , handleAddGreen);
+blue.addEventListener('click' , handleAddGreen);
+
---
+++ Exercice_4/script.js
@@ -0,0 +1,16 @@
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirmPassword');
+const button = document.querySelector('#button')
+
+button.addEventListener('click', handleVerifyPassword);
+
+function handleVerifyPassword(){
+    if(password.value === confirmPassword.value){
+        password.style.border = "3px solid green"
+        confirmPassword.style.border = "3px solid green"
+    }
+    else {password.value != confirmPassword.value
+        password.style.border = "3px solid red"
+        confirmPassword.style.border = "3px solid red"
+    }
+};
